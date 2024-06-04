8807098b1297bc0d112847b9d34b9f4a05c7dc76
Liam <liam.gloggner@hotmail.com>
2024-06-04 10:05:56 +0200

    UI Erstellt

---
+++ TxtRpg/src/ch/teko/gloggnel/MainView.java
@@ -0,0 +1,10 @@
+package ch.teko.gloggnel;
+
+import javax.swing.*;
+
+public class MainView {
+    private JButton kampfButton;
+    private JButton skillsButton;
+    private JButton shopButton;
+    private JTextField hierKönntenIhreStatsTextField;
+}
